5909270b2ba94b74d9a7b99a2b6a75eec5bf7b37
DB Thompson <dbthompson.au@gmail.com>
2020-01-20 10:59:53 +1100

    Initial commit - src cleared and reset

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./../styles/App.css";
+
+class App extends Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+export default App;
