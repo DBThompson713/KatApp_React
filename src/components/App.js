f86792d6fb5204b25c0598f249efe0b902385f14
DB Thompson <dbthompson.au@gmail.com>
2020-01-20 11:32:53 +1100

    added required packages, updated git ignore for env, imported packages as initially required

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./../styles/App.css";
 
 class App extends Component {
