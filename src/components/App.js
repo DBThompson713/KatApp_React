bb8419f52b40b67d032bf5ec8cea0faaed83927e
DB Thompson <dbthompson.au@gmail.com>
2020-01-21 22:28:58 +1100

    react bootstrap added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./../styles/App.css";
+import NavbarMain from "./Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <>
+        <NavbarMain />
+      </>
+    );
   }
 }
 
