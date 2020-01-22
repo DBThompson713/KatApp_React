0d1378c3752abbdb32b5eddbc44b68ef3a709a73
chelseal <chelseablove@gmail.com>
2020-01-22 15:28:56 +1100

    Added Faqs content + styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./../styles/App.css";
+import Faqs from "./pages/FaqPage"
 
 class App extends Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return(
+      <>
+    <Faqs />
+      </>
+    );
   }
 }
 
