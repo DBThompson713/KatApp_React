5bff8f5d24e9199f2eb66f8557005c7172d21749
DB Thompson <dbthompson.au@gmail.com>
2020-01-23 11:32:44 +1100

    test

---
+++ src/components/ImageUpload.js
@@ -0,0 +1,37 @@
+import S3FileUpload from "react-s3";
+import ReactS3 from "react-s3";
+import React from "react";
+import { uploadFile } from "react-s3";
+import ReactDom from "react-dom";
+
+const config = {
+  bucketName: "kat-app-images",
+  region: "ap-southeast-2",
+  accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+  secretAccessKey: process.env.REACT_APP_SECRET_KEY
+};
+
+class ImageUpload extends React.Component {
+  constructor() {
+    super();
+  }
+  upload(event) {
+    uploadFile(event.target.files[0], config)
+      .then(data => {
+        console.log(data.location);
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+  render() {
+    return (
+      <>
+        <h3>Image upload test</h3>
+        <input type="file" onChange={this.upload} />
+      </>
+    );
+  }
+}
+
+export default ImageUpload;
