930b65023370f35dfbc63b803b8fad2290fd2a40
DB Thompson <dbthompson.au@gmail.com>
2020-01-23 14:54:52 +1100

    recipe card styling started

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,22 +3,32 @@ import "./../styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Button } from "react-bootstrap";
 import "./../styles/RecipeCard.css";
-import tempImage from "./../assets/Rainbow2.png";
+import tempImage from "./../assets/vietnamese-chicken-rice-paper-rolls.jpeg";
+import heart from "./../assets/heart.png";
+import share from "./../assets/share_icon.png";
+import carrot from "./../assets/carrot.png";
 
 class RecipeCard extends Component {
   render() {
     return (
       <>
         <div class="MainCard">
-          <Card style={{ width: "18rem" }}>
+          <Card style={{ width: "20rem" }}>
             <Card.Img id="mainRecipeImage" variant="top" src={tempImage} />
-            <Card.Body>
-              <Card.Title>Some Kind of Food</Card.Title>
+            <Card.Body id="card-color">
+              <Card.Title>
+                Some Kind of Food
+                <img id="favourite" src={heart} />
+              </Card.Title>
+
+              <Card.Subtitle>
+                This delicious and healthy meal requires little prep time and is
+                always a hit!
+              </Card.Subtitle>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                42 <img id="carrot" src={carrot} />
+                <img id="favourite" src={share} />
               </Card.Text>
-              <Card.Text> Carrot yums</Card.Text>
             </Card.Body>
           </Card>
         </div>
