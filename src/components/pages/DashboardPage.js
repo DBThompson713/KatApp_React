680cbfaabc776150b8109be8529a03de8d316974
DB Thompson <dbthompson.au@gmail.com>
2020-01-28 13:00:47 +1100

    user Dashboard added

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,9 +1,14 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import "normalize.css";
 import "./../../styles/Dashboard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import { Container, Row, Col, Button } from "react-bootstrap";
 import { useAuth0 } from "../../react-auth0-spa";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import favRecipeImage from "./../../assets/foodHeart.jpeg";
+import allRecipeImage from "./../../assets/food1.jpeg";
+import featRecipeImage from "./../../assets/food2.jpeg";
+import { NavLink } from "react-router-dom";
 
 const Dashboard = () => {
   const { user } = useAuth0();
@@ -18,7 +23,53 @@ const Dashboard = () => {
   return (
     <Fragment>
       <div id="dashboard-container">
-        <h2>{user.nickname}'s Dashboard</h2>
+        <Row id="DashboardRow1">
+          <h2>{user.nickname}'s Dashboard</h2>
+        </Row>
+        <Row id="DashboardRow2">
+          <Col xs={12} md={4}>
+            <NavLink to="/FavouriteRecipesPage">
+              <Card style={{ width: "17rem" }}>
+                <Card.Img variant="top" src={favRecipeImage} />
+                <Card.Body>
+                  <Card.Title>Favourite Recipes</Card.Title>
+                  <Card.Text>
+                    Click here to see your Favourite Recipes
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </NavLink>
+          </Col>
+          <Col xs={12} md={4}>
+            <NavLink to="/AllRecipesPage">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={allRecipeImage} />
+                <Card.Body>
+                  <Card.Title>All Recipes</Card.Title>
+                  <Card.Text>
+                    Need some inspiration? Click here to see all recipes
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </NavLink>
+          </Col>
+          <Col xs={12} md={4}>
+            <NavLink to="/FeaturedRecipesPage">
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={featRecipeImage} />
+                <Card.Body>
+                  <Card.Title>Featured Recipes</Card.Title>
+                  <Card.Text>See some of Kats Favourite Recipes</Card.Text>
+                </Card.Body>
+              </Card>
+            </NavLink>
+          </Col>
+        </Row>
+        <Row id="DashboardRow3">
+          <NavLink to="/NewRecipePage">
+            <Button>Add a New Recipe</Button>
+          </NavLink>
+        </Row>
       </div>
     </Fragment>
   );
