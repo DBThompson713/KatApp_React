3033057f7e2cbb3a1bfe1ed66549feebda274279
chelseal <chelseablove@gmail.com>
2020-02-04 11:21:47 +1100

    Updated styling

--- src/components/pages/DiscoverPage.js
+++ src/components/pages/DiscoverPage.js
@@ -44,14 +44,12 @@ const Discover = () => {
           <h4>How can I add a recipe?</h4>
           <p>
           Click here to add a recipe.
-
+{/* List item dot points are turned off to protect the recipe ingredients form, <li></li> is replaced with <p>-</p> */}
           Every recipe added to Well Kids will need the following input (text).
-          <ul>
-            <li>Title</li>
-            <li>Description</li>
-            <li>Ingredients (entered separately)</li>
-            <li>Steps (entered separately)</li>
-          </ul>
+          <p>- Title</p>
+          <p>- Description</p>
+          <p>- Ingredients (entered separately)</p>
+          <p>- Steps (entered separately)</p>
           </p>
         </div>
         <div>
@@ -63,52 +61,59 @@ const Discover = () => {
         </div>
         <h4>Healthy Lunchbox Snacks by Live Love Nourish</h4>
         <p>
-        As a teacher with a passion for real food and nourishment for kids, I understand the importance of healthy, yet easy, school lunchbox ideas. Casey-Lee Lyons from Live Love Nourish outlines some amazing ideas in her online article “Easy and Healthy Back to School Lunch Box Ideas”. Here is a quick summary of what to include in a healthy lunchbox that will ensure kids are satiated and focussed for the day. Follow <a href="https://www.livelovenourish.com.au/easy-healthy-back-to-school-lunch-box-ideas/" target="_blank">this link</a> to read the full article! 
+        As a teacher with a passion for real food and nourishment for kids, I understand the importance of healthy, yet easy, school lunchbox ideas. Casey-Lee Lyons from Live Love Nourish outlines some amazing ideas in her online article “Easy and Healthy Back to School Lunch Box Ideas”.
+        </p>
+      <p className="paragraphBreak">
+        Here is a quick summary of what to include in a healthy lunchbox that will ensure kids are satiated and focussed for the day. Follow <a href="https://www.livelovenourish.com.au/easy-healthy-back-to-school-lunch-box-ideas/" target="_blank">this link</a> to read the full article! 
         </p>
         <div>
         <h4>Important Tips</h4>
         <p>Some important tips before we get started:</p>
         <p>
-        <ul>
-          <li>Food should be fun</li>
-          <li>Keep it simple</li>
-          <li>Stress Less</li>
-        </ul>
+          <p>- Food should be fun</p>
+          <p>- Keep it simple</p>
+          <p>- Stress Less</p>
         </p>
         <h4>Savoury Main Items</h4>
         <p>
-        <ul>
-          <li>Frittata or egg and veggie muffins</li>
-          <li>Fritters (you can make these with egg and veggies or for an egg free option use chick- pea our + water in place of eggs)</li>
-          <li>Homemade sushi (you can make this with quinoa too. If using quinoa mash a small amount of hummus or avocado into the quinoa so it sticks together).</li>
-          <li>Spaghetti bolognese muffins (make these by whisking leftover spaghetti bolognese with 1-2 eggs and bake in muffin trays). You can also do this to make a fried rice slice, mix fried rice with whisked egg and bake in a slice tray for a lunchbox friendly way to serve fried rice).</li>
-        </ul>
+          <h5>Frittata or egg and veggie muffins</h5>
+          <h6>Fritters</h6>
+          <p>You can make these with egg and veggies or for an egg free option use chick- pea our + water in place of eggs</p>
+          <h6>Homemade sushi</h6>
+          <p>You can make this with quinoa too. If using quinoa mash a small amount of hummus or avocado into the quinoa so it sticks together.</p>
+          <h6>Spaghetti bolognese muffins</h6>
+          <p>Make these by whisking leftover spaghetti bolognese with 1-2 eggs and bake in muffin trays). You can also do this to make a fried rice slice, mix fried rice with whisked egg and bake in a slice tray for a lunchbox friendly way to serve fried rice.</p>
         </p>
-        <h4>Whole Fruit and Vegetables</h4>
+        <h5>Whole Fruit and Vegetables</h5>
         <p>
-        <ul>
-          <li>Cut fruit into fun shapes using a cookie cutter.</li>
-          <li>Name veggie sticks in a fun way. Such as wizard swords for asparagus spears,</li>
-          <li>Lightly steam and cool some vegetable sticks. Some kids don’t like raw veggies but will eat softened carrot sticks or softened beans for example.</li>
-          <li>Get kids to pick their favourite fruits or get them involved in chopping and preparing to encourage them to eat them.</li>
-        </ul>
+          <h6>Fun shapes</h6>
+          <p>Cut fruit into fun shapes using a cookie cutter.</p>
+          <h6>Fun name</h6>
+          <p>Name veggie sticks in a fun way. Such as wizard swords for asparagus spears,</p>
+          <h6>Almost raw</h6>
+          <p>Lightly steam and cool some vegetable sticks. Some kids don’t like raw veggies but will eat softened carrot sticks or softened beans for example.</p>
+          <h6>Prep together</h6>
+          <p>Get kids to pick their favourite fruits or get them involved in chopping and preparing to encourage them to eat them.</p>
         </p>
-        <h4>Snacks</h4>
+        <h5>Snacks</h5>
         <p>
-        <ul>
-          <li>Dips. These are a fun item in kid’s lunch boxes. Such a hummus, guacamole or their favourite. Serve with crackers or veggie sticks. WholeLife has a great range of organic and vegan dips.</li>
-          <li>Yoghurt with mixed berries. Choose a yogurt with no added sugar. We use unsweetened coconut yoghurt.</li>
-          <li>Pikelets are a great fun food ideal for little hands and when made with wholesome ingredients they can be a healthy option too. I make ours from buckwheat our and top with homemade chia seed jam or use this recipe here.</li>
-          <li>Mini muffins. Use whole fruit to sweeten vs adding re ned sugar. We love making these mini coconut banana muffins.</li>
-        </ul>
+        <h6>Dips</h6>
+          <p>These are a fun item in kid’s lunch boxes. Such a hummus, guacamole or their favourite. Serve with crackers or veggie sticks. WholeLife has a great range of organic and vegan dips.</p>
+          <h6>Yoghurt with mixed berries</h6>
+          <p>Choose a yogurt with no added sugar. We use unsweetened coconut yoghurt.</p>
+          <h6>Pikelets</h6>
+          <p>Pikelets are a great fun food ideal for little hands and when made with wholesome ingredients they can be a healthy option too. I make ours from buckwheat our and top with homemade chia seed jam or use this recipe here.</p>
+          <h6>Mini muffins</h6>
+          <p>Use whole fruit to sweeten vs adding re ned sugar. We love making these mini coconut banana muffins.</p>
         </p>
-        <h4>Quick and Convenient</h4>
+        <h5>Quick and Convenient</h5>
         <p>
-        <ul>
-          <li>Snack bars – There are lots of healthy whole food bars available, just be mindful of a few key things to choose a healthier one. Look for no added refined sugars and preservatives.</li>
-          <li>Bliss balls – Typically bliss balls are made with nuts, seeds, coconut and dates or rice malt syrup. Look for ones without added preservatives and if your school is nut free, check the ingredient label. You can also make these at home!</li>
-          <li>Roasted chickpeas or fava beans – These are a tasty snack providing a source of protein and fibre.</li>
-        </ul>
+          <h6>Snack bars</h6>
+            <p>There are lots of healthy whole food bars available, just be mindful of a few key things to choose a healthier one. Look for no added refined sugars and preservatives.</p>
+            <h6>Bliss balls</h6>
+          <p>Typically bliss balls are made with nuts, seeds, coconut and dates or rice malt syrup. Look for ones without added preservatives and if your school is nut free, check the ingredient label. You can also make these at home!</p>
+          <h6>Roasted chickpeas or fava beans</h6>
+          <p>These are a tasty snack providing a source of protein and fibre.</p>
         </p>
         </div>
       </section>
